fa5d8d6a971b9f6482286795c8a5118b5ff4217d
lisa1209 <43108731+lisa1209@users.noreply.github.com>
2019-06-01 00:10:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,61 @@
-import json
-import os
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
 
-from flask import Flask
-from flask import request
-from flask import make_response
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+//const {Card, Suggestion} = require('dialogflow-fulfillment');
 
-# Flask app should start in global layout
-app = Flask(__name__)
+const admin = require('firebase-admin');
+admin.initializeApp(
+  {
+  credential: admin.credential.applicationDefault(),
+ databaseURL: 'https://ginraibot.firebaseio.com/'
+}
+);
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-    req = request.get_json(silent=True, force=True)
-    res = processRequest(req)
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-   const functions = require('firebase-functions');
-   const {WebhookClient} = require('dialogflow-fulfillment');
-   //const {Card, Suggestion} = require('dialogflow-fulfillment');
+  function orr(agent){
+  const nameParam = agent.parameters.namee;
+   const nameParam2 = agent.parameters.namee2;
+    const namParam3 = agent.parameters.number;
+    const j = agent.parameters.kj;
 
-   const admin = require('firebase-admin');
-   admin.initializeApp(
-     {
-     credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://ginraibot.firebaseio.com/'
-   }
-   );
 
-   process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+  const name = nameParam;
+    const name2 = nameParam2;
+    const numm = namParam3;
+    const kj = j;
 
-   exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-     const agent = new WebhookClient({ request, response });
-     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-     function orr(agent){
-     const nameParam = agent.parameters.namee;
-      const nameParam2 = agent.parameters.namee2;
-      // const nameParam3 = agent.parameters.namee2;
+    //const name3 = nameParam3;
 
-     const name = nameParam;
-       const name2 = nameParam2;
-       //const name3 = nameParam3;
-
-       agent.add('รายการที่สั่งคือ'+name+name2 || name2+name +'ค่ะ');
-
-       return admin.database().ref('/names').push({name: name+name2 || name2+name }).then((snapshot) =>{
-         console.log('database write sucessful:' +snapshot.ref.toString());
-     }
-     );
-     }
+    agent.add('รายการที่สั่งคือ'+(name+name2 || name2+name )+numm+kj+'ค่ะ');
 
 
 
+    return admin.database().ref('/names/data').push({name: name+name2 || name2+name ,num:numm+kj}).then((snapshot) =>{
+      console.log('database write sucessful:' +snapshot.ref.toString());
+  }
+  );
 
-     function fallback(agent) {
-       agent.add(`I didn't understand`);
-       agent.add(`I'm sorry, can you try again?`);
-     }
-
-     let intentMap = new Map();
-     intentMap.set('Order',orr);
-   intentMap.set('cancer',orr);
-     agent.handleRequest(intentMap);
-   });
+  }
 
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
 
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+  let intentMap = new Map();
+  intentMap.set('Order',orr);
+intentMap.set('cancer',orr);
+  agent.handleRequest(intentMap);
+});
